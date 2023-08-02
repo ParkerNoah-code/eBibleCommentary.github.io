dcf389bb56ef8daacc2cc035c0d436fd5c065b2c
ParkerNoah <72557251+ParkerNoah@users.noreply.github.com>
2023-08-02 17:40:47 -0400

    edit

--- functions.js
+++ functions.js
@@ -1,6 +1,11 @@
 // Function to play the audio and show the custom controls
 function playAudioWithControls() {
-    const audio = document.getElementById('audio-element');
+  const audio = document.getElementById("audio-element");
+  if (audio.controls) {
+    audio.controls = false;
+    audio.pause();
+  } else {
     audio.controls = true;
     audio.play();
   }
+}
