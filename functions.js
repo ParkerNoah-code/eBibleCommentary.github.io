6281c3c6233765d3769806422ae79b29fefcb27a
ParkerNoah <72557251+ParkerNoah@users.noreply.github.com>
2023-08-03 23:33:35 -0400

    edit

--- functions.js
+++ functions.js
@@ -9,3 +9,15 @@ function playAudioWithControls() {
     audio.play();
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the button and the content
+  const revealButton = document.getElementById("text-button");
+  const content = document.getElementById("content");
+
+  // Add an event listener to the button
+  revealButton.addEventListener("click", function () {
+    // Toggle the 'hidden' class on the content div
+    content.classList.toggle("hidden");
+  });
+});
