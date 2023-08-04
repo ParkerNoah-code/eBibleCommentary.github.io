2387a387d53b6f0fc63bba2e59fb67aa54679b25
ParkerNoah <72557251+ParkerNoah@users.noreply.github.com>
2023-08-04 15:17:35 -0400

    edit

--- create_files.py
+++ create_files.py
@@ -1,7 +1,7 @@
-with open("D:\WritingProject\minininja0412.github.io\OldTestament\PoeticWisdom\Psalms\content.txt", "r") as content_file:
+with open("content.txt", "r") as content_file:
     content = content_file.read()
 
 for i in range(1, 151):
-    filename = f"OldTestament\PoeticWisdom\Psalms\Commentary{i}.html"
+    filename = f"OldTestament\PoeticWisdom\Psalms\BackgroundCommentary{i}.html"
     with open(filename, "w") as file:
         file.write(content)
