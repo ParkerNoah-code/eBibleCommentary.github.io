c8ae458718f2a6af5598c764a1d97b995a432387
ParkerNoah <72557251+ParkerNoah@users.noreply.github.com>
2023-08-05 22:28:10 -0400

    edit

--- functions.js
+++ functions.js
@@ -1,4 +1,3 @@
-// Function to play the audio and show the custom controls
 function playAudioWithControls() {
   const audio = document.getElementById("audio-element");
   if (audio.controls) {
@@ -14,6 +13,29 @@ document.addEventListener("DOMContentLoaded", function () {
   // Get references to the button and the content
   const revealButton = document.getElementById("text-button");
   const content = document.getElementById("content");
+
+  // Add an event listener to the button
+  revealButton.addEventListener("click", function () {
+    // Toggle the 'hidden' class on the content div
+    content.classList.toggle("hidden");
+  });
+});
+
+function playMusicWithControls() {
+  const audio = document.getElementById("music-element");
+  if (audio.controls) {
+    audio.controls = false;
+    audio.pause();
+  } else {
+    audio.controls = true;
+    audio.play();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the button and the content
+  const revealButton = document.getElementById("music-button");
+  const content = document.getElementById("music");
 
   // Add an event listener to the button
   revealButton.addEventListener("click", function () {
