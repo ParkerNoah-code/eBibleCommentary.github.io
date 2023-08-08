3ca2b5ff742b243d9dc53092b499ce388e4139e5
ParkerNoah <72557251+ParkerNoah@users.noreply.github.com>
2023-08-08 14:57:54 -0400

    edit

--- create_files.py
+++
@@ -1,7 +0,0 @@
-with open("content.txt", "r") as content_file:
-    content = content_file.read()
-
-for i in range(1, 151):
-    filename = f"OldTestament\PoeticWisdom\Psalms\SermonicCommentary{i}.html"
-    with open(filename, "w") as file:
-        file.write(content)
