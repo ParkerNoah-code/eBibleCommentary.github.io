b7d7a95d0ad4456b704207deaa755fe793251221
ParkerNoah <72557251+ParkerNoah@users.noreply.github.com>
2024-03-01 23:02:37 -0500

    edit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function loadContent(contentName) {
   if (!contentName) return; // Do nothing if no content is selected
 
   // Fetch the HTML file based on contentName
-  fetch(`${contentName}.html`)
+  fetch(`Bible\\${contentName}.html`)
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -16,4 +16,14 @@ function loadContent(contentName) {
     .catch((error) => {
       console.error("Failed to load content:", error);
     });
+}
+
+function showContent(contentId) {
+  // Hide all content first
+  document.querySelectorAll("#content-area > div").forEach(function (content) {
+    content.style.display = "none";
+  });
+
+  // Show the selected content
+  document.getElementById(contentId).style.display = "block";
 }
